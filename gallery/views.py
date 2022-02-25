a73dd0a3ef76a2ccaf7d6e28ff90fde3d5659643
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-02-25 23:58:03 +0300

    create views

--- gallery/views.py
+++ gallery/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
+def all_photos(request):
+   
+    return HttpResponse('This is my where all my images will be displayed. also the homepage')
+
+def photo_description(request, image_id):
+    
+    return HttpResponse('This is my image description view')
+
+def search_results(request):
+   return HttpResponse('This is my where the searched categories will be displayed')
