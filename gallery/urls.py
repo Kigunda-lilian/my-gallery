d18af26037ba23367c5441d757a55ccb918e5bcc
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-02-26 11:50:34 +0300

    add views to urlpatterns

--- gallery/urls.py
+++ gallery/urls.py
@@ -22,5 +22,10 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', views.all_photos, name='allphotos'),
+    url(r'^photos/(\d+)', views.photo_description,name = 'photo'),
+    url(r'^search/', views.search_results, name = 'search_results'),
 ]
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
