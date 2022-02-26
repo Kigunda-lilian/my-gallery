b94dc01713f3db8df5f5300d7ac5f657b285eef9
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-02-26 16:48:18 +0300

    create  test_instance test

--- gallery/tests.py
+++ gallery/tests.py
@@ -5,3 +5,6 @@ from .models import categories,location,Gallery
 class categoriesTestCase(TestCase):
     def setUp(self):
         self.fashion = categories(name = 'fashion')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.fashion, categories))
