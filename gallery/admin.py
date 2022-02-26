4b2bd9e73ef4b871afa92ec5c1616bf8375a901d
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-02-26 17:33:58 +0300

    add models to the admin page

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Gallery,location,categories
+
 
 # Register your models here.
+admin.site.register(Gallery)
+admin.site.register(location)
+admin.site.register(categories)
