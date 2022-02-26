465532a8859c46a48d9bc387cc9cb3e3d457f637
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-02-27 01:21:45 +0300

    import the project settings from the django.conf module and a static function.  add to the urlpatterns a new static route that references the location to the uploaded files.

--- gallery/urls.py
+++ gallery/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     url(r'^search/', views.search_results, name = 'search_results'),
     url(r'^profile/', views.profile, name = 'profile'),
 ]
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
