ded6cd4f517c246cfd3442e8f450ed7b12e6f0ed
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-02-26 15:37:46 +0300

    create location model and run migrations

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,11 @@ from django.db import models
 class categories(models.Model):
     name = models.CharField(max_length=46)
 
-    
+    def __str__(self):
+            return self.name
+        
+class location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
