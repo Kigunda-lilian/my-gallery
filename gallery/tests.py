95a244db39fcf8bf47dac320c9adf88d7f17c5f4
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-02-26 16:45:47 +0300

    create Categories test class for the categories module. create a setUp method for creating an instance of the categories class before every test.

--- gallery/tests.py
+++ gallery/tests.py
@@ -2,3 +2,6 @@ from django.test import TestCase
 from .models import categories,location,Gallery
 
 # Create your tests here.
+class categoriesTestCase(TestCase):
+    def setUp(self):
+        self.fashion = categories(name = 'fashion')
